6eddb3bf6b8e843dd3ce9620bc751bad33f6bf28
Crypt06545 <mehedihasansagor2005@gmail.com>
2025-03-29 15:31:02 +0600

    create data model

--- src/app.js
+++ src/app.js
@@ -9,9 +9,17 @@ app.use(
     credentials: true,
   })
 );
+// middlewares
 app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ credentials: true, limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//import routes
+import userRouter from "./routes/user.routes.js";
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
 
 export default app;
--- src/models/user-model.js
+++ src/models/user-model.js
@@ -1,26 +1,37 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const UserSchema = Schema(
+const UserSchema = new Schema(
   {
     username: {
       type: String,
       required: true,
       unique: true,
       lowercase: true,
+      trim: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
       lowercase: true,
+      trim: true,
     },
     password: {
       type: String,
       required: true,
     },
-    
+
+    // cloudnary
+    // avatar: {
+    //   type: String,
+    //   required: true,
+    // },
+
     isAdmin: {
       type: Boolean,
+      default: false,
     },
     orders: [
       {
@@ -28,8 +39,50 @@ const UserSchema = Schema(
         ref: "Product",
       },
     ],
+    refreshToken: {
+      type: String,
+    },
   },
   { timestamps: true }
 );
 
+// Password hashing middleware
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Password verification method
+UserSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// jwt token generate
+UserSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+// jwt refresh token
+UserSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 export const User = mongoose.model("User", UserSchema);
